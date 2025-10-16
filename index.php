3094be12219249598e08b5cb0ceaf9a37b925372
Nazarudin <160707783+nazarudin9504@users.noreply.github.com>
2025-10-16 21:00:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 </php
-echo "<p>test</p>";
+echo "<h1>Halo dari Oracle Cloud!</h1>";
+echo "<p>Develpoment dengan Nginx dan Ci/CD Github Action</p>"
+echo "<p>File yang diambil dari repository GitHub</p>"
 ?>
